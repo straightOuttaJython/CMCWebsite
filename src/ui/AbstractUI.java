d141ce8f77a6bc5964905b67cc9c2a53d05375a5
Alex Seefeldt <atseefeldt@nx.csbsju.edu>
2016-03-09 20:49:50 -0600

    This is my first commit

---
+++ src/ui/AbstractUI.java
@@ -0,0 +1,20 @@
+package ui;
+
+public interface AbstractUI {
+	
+	/**
+	 * Show menu
+	 */
+	public void viewMenu();
+	
+	/**
+	 * Log Person out of session
+	 */
+	public void logout();
+	
+	/**
+	 * Reset all fields in current form.
+	 */
+	public void resetForm();
+
+}
---
+++ src/ui/AdminUI.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+package ui;
+
+/**
+ * @author Alex Seefeldt
+ *
+ */
+public class AdminUI implements AbstractUI {
+	
+	/**
+	 * Create a new AdminUI.
+	 */
+	public AdminUI() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Show Admin menu
+	 */
+	public void viewMenu() {
+		
+	}
+	
+	/**
+	 * Show Manage Schools menu
+	 */
+	public void manageSchools() {
+		
+	}
+	
+	/**
+	 * Log Admin out of session
+	 */
+	public void logout() {
+		
+	}
+	
+	/**
+	 * Reset all fields in current form.
+	 */
+	public void resetForm() {
+		
+	}
+	
+	/**
+	 * Add the given School object to the database.
+	 * @param school the new School to be added
+	 */
+	public void addSchool(School school) {
+		
+	}
+	
+	/**
+	 * Update the given School with the data inside it.
+	 * @param school the updated School
+	 */
+	public void editSchool(School school) {
+		
+	}
+	
+	
+	/**
+	 * Report to user that the School could not be added.
+	 */
+	private void denyAddSchool() {
+		
+	}
+	
+	/**
+	 * Leave the Edit School form and return to upper menu.
+	 */
+	public void cancelEditSchool() {
+		
+	}
+
+	/**
+	 * 
+	 */
+	private void failureToEditSchool() {
+		
+	}
+
+	
+	/**
+	 * 
+	 */
+	public void addAUser() {
+		
+	}
+
+	/**
+	 * @param user
+	 */
+	public void editUser(Person user) {
+		
+	}
+	
+	/**
+	 * @param user
+	 */
+	public void deactivate(Person user) {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void addUser() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private void denyAddUser() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private void failureToEditUser() {
+		
+	}
+
+}
