1f14758b71a8b5c05b8f3909b2eae75b0dd07fcb
erinqueme <erinquemeeastend@gmail.com>
2016-03-09 20:55:24 -0600

    Everything should now work except the return statements and adding Person objects into specific classes or others

---
+++ src/cmc/LoginController.java
@@ -0,0 +1,21 @@
+package cmc;
+
+/**
+ * The LoginController class controls how/when a Person would log in.
+ * 
+ * @author Matthew
+ * @version March 6, 2016
+ */
+public class LoginController {
+	/**
+	 * The login method users an entered username and password to find a Person that matches those parameters and returns that Person.
+	 * 
+	 * @param username, the username that is entered.
+	 * @param password, the password that is entered.
+	 * @return Person object that was found based on the usernam and password. 
+	 */
+	public Person login(String username, String password) 
+	{
+		
+	}
+}
