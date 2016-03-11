ad8d94c5d7b62c915691a030869b293b7b6a8dd8
Matthew Kounniyom <makounniyom@csbsju.edu>
2016-03-10 19:53:00 -0600

    Added another needed jar to make the database work

--- src/cmc/controller/LoginController.java
+++ src/cmc/controller/LoginController.java
@@ -1,5 +1,8 @@
 package cmc.controller;
 
+import cmc.entity.Person;
+import dblibrary.project.csci230.UniversityDBLibrary;
+
 /**
  * The LoginController class controls how/when a Person would log in.
  * 
@@ -12,10 +15,15 @@ public class LoginController {
 	 * 
 	 * @param username, the username that is entered.
 	 * @param password, the password that is entered.
-	 * @return Person object that was found based on the usernam and password. 
+	 * @param database, the database to search through.
+	 * @return Person object that was found based on the username and password. 
 	 */
-	public Person login(String username, String password) 
+	public Person login(String username, String password, UniversityDBLibrary dataBase) 
 	{
-		
+		String user = username;
+		String pass = password;
+		String[][] array = dataBase.user_getUsers();
+		System.out.println(array.toString());
+		return new Person();
 	}
 }
