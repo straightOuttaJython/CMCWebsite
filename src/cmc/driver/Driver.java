234b05fbf2dd0da007062dad1de719703d05b7c9
ejqueme <ejqueme@nx.csbsju.edu>
2016-03-12 02:44:34 -0600

    Placing Driver back into the package

---
+++ src/cmc/driver/Driver.java
@@ -0,0 +1,51 @@
+package cmc.driver;
+
+import cmc.controller.LoginController;
+import cmc.entity.Person;
+import cmc.ui.AdminUI;
+import cmc.ui.LoginUI;
+import cmc.ui.UserUI;
+import dblibrary.project.csci230.UniversityDBLibrary;
+
+public class Driver {
+
+	/**
+	 * Used to name the database and is the login username.
+	 */
+	private static String nameDB = "straightou";
+	
+	/**
+	 * Database password.
+	 */
+	private static String passDB = "adem4";
+	
+	/**
+	 * Generic main method to drive the classes.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// tests login
+		LoginUI login = new LoginUI();
+		login.login();
+		
+		//admin stuff
+		AdminUI admin = new AdminUI();
+		admin.viewMenu();
+		
+
+		//System.out.println("********** UPDATED **********");
+		admin.manageSchools();
+		
+		//user ui stuff
+		UserUI user = new UserUI(login.getLoggedPerson());
+		user.viewMenu();
+		
+
+		UserUI user1 = new UserUI(login.getLoggedPerson());
+		user1.viewMenu();
+		user1.manageUserProfile();
+		user1.logout(login);
+	}
+}
